db8f935ecacc2c893a9d1f6a962cb4135fcbc5f9
jhon anderson <apxanderson02@gmail.com>
2022-01-27 17:24:05 -0500

    scroll animacion

--- js/script.js
+++ js/script.js
@@ -1,39 +1,44 @@
 $(document).ready(function () {
+  //$("body").css("overflow", "hidden");
   let observar = [true, true, true, true];
-  const elementosObservados  =  [".redes", "#about-us", "#services" ,"#contact"]
+  const elementosObservados = [".redes", "#about-us", "#services", "#contact"];
   let guia = true;
 
-  const observarAnimar =(elemento)=>{
-	const options = {
-		threshold: 1.0,
-	  };
-	  const cb = (entries) => {
-		if (!observar[elemento]) return;
-	
-		entries.forEach((entry) => {
-		  if (entry.isIntersecting) {
-			observar[elemento] = false;
-			document.querySelector(elementosObservados[elemento]).classList.toggle("animate__animated");
-			document
-			  .querySelector( elementosObservados[elemento])
-			  .classList.toggle("animate__bounceInDown");
-			document.querySelector(elementosObservados[elemento]).classList.toggle("animate__delay-1s");
-		  }
-		});
-	  };
+  const observarAnimar = (elemento) => {
+    const options = {
+      threshold: 1.0,
+    };
+    const cb = (entries) => {
+      if (!observar[elemento]) return;
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          observar[elemento] = false;
 
-	  let observer = new IntersectionObserver(cb, options);
-	  observer.observe(document.querySelector(elementosObservados[elemento]));
-  }
+          document
+            .querySelector(elementosObservados[elemento])
+            .classList.toggle("animate__animated");
+          document
+            .querySelector(elementosObservados[elemento])
+            .classList.toggle("animate__bounceInDown");
+          document
+            .querySelector(elementosObservados[elemento])
+            .classList.toggle("animate__delay-1s");
+        }
+      });
+    };
 
-  elementosObservados.map((elemento, index) =>{
-	observarAnimar(index)
-  })
+    let observer = new IntersectionObserver(cb, options);
+    observer.observe(document.querySelector(elementosObservados[elemento]));
+  };
+
+  elementosObservados.map((elemento, index) => {
+    observarAnimar(index);
+  });
 
   const verify = (scrollPosition) => {
-	if (window.scrollY !== scrollPosition) {
-	  guia = false;
-	}
+    if (window.scrollY !== scrollPosition) {
+      guia = false;
+    }
   };
   const guiar = () => {
     if (!guia) return;
@@ -62,11 +67,12 @@ $(document).ready(function () {
       window.location.href = "#inicio";
       let scrollPosition = window.screenY;
       verify(scrollPosition);
+      $("body").css("overflow", "auto");
     }, 5000 * 4);
+
     if (!guia) return;
   };
   guiar();
-
   $(function () {
     var windowH = $(window).height();
     var wrapperH = $(".hero").height();
